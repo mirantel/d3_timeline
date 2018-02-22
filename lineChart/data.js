9abb602ac3d755f8bf64815d6814856288809432
Svitlana Pylypenko <svitlana.pylypenko@epam.com>
2018-02-22 14:13:48 +0200

    Add Data

---
+++ lineChart/data.js
@@ -0,0 +1,76 @@
+// Graph Tests Data
+var myRubyData = [{
+  "score": 85.71,
+  "date": "2018-01-20T13:07:15.038Z",
+  "created_by": "Tesla"
+},{
+  "score": 61.9,
+  "date": "2018-01-21T11:16:35.002Z",
+  "created_by": "Lana"
+},{
+  "score": 61.9,
+  "date": "2018-01-30T11:19:40.537Z",
+  "created_by": "Lana"
+},{
+  "score": 61.9,
+  "date": "2018-02-02T13:51:39.589Z",
+  "created_by": "Lana"
+},{
+  "score": 61.9,
+  "date": "2018-02-03T13:52:52.941Z",
+  "created_by": "Lana"
+},{
+  "score": 61.9,
+  "date": "2018-02-04T14:08:07.335Z",
+  "created_by": "Lana"
+},{
+  "score": 66.67,
+  "date": "2018-02-05T11:01:53.887Z",
+  "created_by": "Lana"
+},{
+  "score": 66.67,
+  "date": "2018-02-07T11:03:06.547Z",
+  "created_by": "Lana"
+},{
+  "score": 76.19,
+  "date": "2018-02-08T11:03:40.912Z",
+  "created_by": "Lana"
+},{
+  "score": 80.95,
+  "date": "2018-02-09T11:04:14.111Z",
+  "created_by": "Lana"
+},{
+  "score": 80.95,
+  "date": "2018-02-10T11:04:57.099Z",
+  "created_by": "Lana"
+},{
+  "score": 80.95,
+  "date": "2018-02-11T11:06:37.212Z",
+  "created_by": "Lana"
+},{
+  "score": 80.95,
+  "date": "2018-02-12T11:27:29.440Z",
+  "created_by": "Lana"
+},{
+  "score": 23.81,
+  "date": "2018-02-13T16:16:50.004Z",
+  "created_by": "Lana"
+},{
+  "score": 23.81,
+  "date": "2018-02-15T16:16:54.905Z",
+  "created_by": "Lana"
+}];
+
+var myRubyData2 = [{
+  "score": 85.71,
+  "date": "2018-01-20T13:07:15.038Z",
+  "created_by": "Mars"
+},{
+  "score": 61.9,
+  "date": "2018-01-21T11:16:35.002Z",
+  "created_by": "Lana"
+},{
+  "score": 61.9,
+  "date": "2018-01-30T11:19:40.537Z",
+  "created_by": "Lana"
+}];
