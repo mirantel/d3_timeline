0ce2671af7427803dbec5a14c148fdda82a8e1f6
mirantel <svitlana.mirantel@gmail.com>
2018-03-06 23:05:59 +0200

    Add rename improve build

--- gulp-tasks/js.js
+++ gulp-tasks/js.js
@@ -3,13 +3,18 @@ const paths = require('./paths');
 const connect = require('gulp-connect');
 const babel = require('gulp-babel');
 const uglify = require('gulp-uglify');
+const rename = require('gulp-rename');
 
 gulp.task('js:build', () => {
   gulp.src([paths.jsSrc])
+    .pipe(gulp.dest(paths.dest))
     .pipe(babel({
-      "presets": ["es2015"]
+      'presets': ['es2015']
     }))
     .pipe(uglify())
+    .pipe(rename({
+      suffix: '.min'
+    }))
     .pipe(gulp.dest(paths.dest))
     .pipe(connect.reload());
 });
