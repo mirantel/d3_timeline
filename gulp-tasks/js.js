df9b797572b002d707c97798eb0f2779b9f24063
Svitlana Pylypenko <svitlana.pylypenko@epam.com>
2018-03-06 19:05:47 +0200

    Add gulp-babel

--- gulp-tasks/js.js
+++ gulp-tasks/js.js
@@ -1,9 +1,11 @@
 const gulp = require('gulp');
 const paths = require('./paths');
 const connect = require('gulp-connect');
+const babel = require('gulp-babel');
 
 gulp.task('js:build', () => {
   gulp.src([paths.jsSrc])
+    .pipe(babel())
     .pipe(gulp.dest(paths.dest))
     .pipe(connect.reload());
 });
